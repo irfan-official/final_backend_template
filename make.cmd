3a37384360b433fb7ae7d579e45673c5c99ca526
Kazi Irfan <irfanofficial.work@gmail.com>
2026-06-22 11:39:48 +0600

    Update make.cmd

--- make.cmd
+++ make.cmd
@@ -4,4 +4,4 @@ REM Get current directory (where command is run)
 set PROJECT_DIR=%cd%
 
 REM Call your TypeScript script
-ts-node "%PROJECT_DIR%\scripts\makeModule.ts" %*
+npx ts-node "%PROJECT_DIR%\scripts\makeModule.ts" %*
